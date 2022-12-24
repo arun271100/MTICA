648df4569281aa83471547f0288cc6aad40c3da6
arun271100 <112647848+arun271100@users.noreply.github.com>
2022-12-24 15:38:40 +0530

    Add files via upload

---
+++ PP_replace1by0.py
@@ -0,0 +1,6 @@
+def replace_1_by_0(n):
+    n=str(n)
+    n=n.replace("1","0")
+    return n
+inp=int(input())
+print(replace_1_by_0(inp))
---
+++ PP_replace6by0.py
@@ -0,0 +1,6 @@
+def replace_6_by_0(n):
+    n=str(n)
+    n=n.replace("6","0")
+    return n
+inp=int(input())
+print(replace_6_by_0(inp))
